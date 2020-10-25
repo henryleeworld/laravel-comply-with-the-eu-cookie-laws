0ab93f64980073f28f52204769c3b080b000fd41
Henry Lee <henryleeworld@gmail.com>
2020-10-26 06:11:15 +0800

    feat:初始化

---
+++ resources/views/vendor/cookieConsent/index.blade.php
@@ -0,0 +1,74 @@
+@if($cookieConsentConfig['enabled'] && ! $alreadyConsentedWithCookies)
+    <style>
+        .js-cookie-consent {
+            position: bottom;
+            top: 0px;
+            padding: 10px;
+            text-align: center;
+            width: 100%;
+            z-index: 9999;
+            background-color: #fffbdb;
+            border-color: #fffacc;
+            border: solid 1px;
+        }
+        .js-cookie-consent-agree {
+            cursor: pointer;
+            background: #008ad7;
+            color: #fff;
+            padding-left: 1em;
+            padding-right: 1em;
+        }
+    </style>
+    @include('cookieConsent::dialogContents')
+    <script>
+
+        window.laravelCookieConsent = (function () {
+
+            const COOKIE_VALUE = 1;
+            const COOKIE_DOMAIN = '{{ config('session.domain') ?? request()->getHost() }}';
+
+            function consentWithCookies() {
+                setCookie('{{ $cookieConsentConfig['cookie_name'] }}', COOKIE_VALUE, {{ $cookieConsentConfig['cookie_lifetime'] }});
+                hideCookieDialog();
+            }
+
+            function cookieExists(name) {
+                return (document.cookie.split('; ').indexOf(name + '=' + COOKIE_VALUE) !== -1);
+            }
+
+            function hideCookieDialog() {
+                const dialogs = document.getElementsByClassName('js-cookie-consent');
+
+                for (let i = 0; i < dialogs.length; ++i) {
+                    dialogs[i].style.display = 'none';
+                }
+            }
+
+            function setCookie(name, value, expirationInDays) {
+                const date = new Date();
+                date.setTime(date.getTime() + (expirationInDays * 24 * 60 * 60 * 1000));
+                document.cookie = name + '=' + value
+                    + ';expires=' + date.toUTCString()
+                    + ';domain=' + COOKIE_DOMAIN
+                    + ';path=/{{ config('session.secure') ? ';secure' : null }}'
+                    + '{{ config('session.same_site') ? ';samesite='.config('session.same_site') : null }}';
+            }
+
+            if (cookieExists('{{ $cookieConsentConfig['cookie_name'] }}')) {
+                hideCookieDialog();
+            }
+
+            const buttons = document.getElementsByClassName('js-cookie-consent-agree');
+
+            for (let i = 0; i < buttons.length; ++i) {
+                buttons[i].addEventListener('click', consentWithCookies);
+            }
+
+            return {
+                consentWithCookies: consentWithCookies,
+                hideCookieDialog: hideCookieDialog
+            };
+        })();
+    </script>
+
+@endif
